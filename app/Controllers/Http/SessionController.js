ff6fffca986013bd281684ffc06bf8ccc41d8bc1
Neylanio Soares <neylaniogomes@gmail.com>
2020-03-01 17:04:51 -0300

    adicionar TASKS

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -8,7 +8,7 @@ const User = use('App/Models/User')
 class SessionController {
   async store ({ request, response, auth }) {
     const { email, password } = request.only(['email', 'password'])
-    const user = await User.findByOrFail('email', email)
+    const user = await User.findBy('email', email)
 
     if (!user) {
       return response.status(401).send({ message: Antl.formatMessage('messages.unauthorized') })
